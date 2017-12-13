3720d1c648f6a07f23245014774a46a7a4514b5f
mski <mskipiotrek@gmail.com>
2017-12-13 02:31:56 +0000

    changed array with letters to array literal its shorter now

--- PL/js/hangman.js
+++ PL/js/hangman.js
@@ -30,44 +30,7 @@ function showPuzzle()
 window.onload = start;
 
 var letters1 = new Array(35);
-
-var letters = new Array(35);
-
-letters[0] = "A";
-letters[1] = "Ą";
-letters[2] = "B";
-letters[3] = "C";
-letters[4] = "Ć";
-letters[5] = "D";
-letters[6] = "E";
-letters[7] = "Ę";
-letters[8] = "F";
-letters[9] = "G";
-letters[10] = "H";
-letters[11] = "I";
-letters[12] = "J";
-letters[13] = "K";
-letters[14] = "L";
-letters[15] = "Ł";
-letters[16] = "M";
-letters[17] = "N";
-letters[18] = "Ń";
-letters[19] = "O";
-letters[20] = "Ó";
-letters[21] = "P";
-letters[22] = "Q";
-letters[23] = "R";
-letters[24] = "S";
-letters[25] = "Ś";
-letters[26] = "T";
-letters[27] = "U";
-letters[28] = "V";
-letters[29] = "W";
-letters[30] = "X";
-letters[31] = "Y"; 
-letters[32] = "Z";
-letters[33] = "Ż";
-letters[34] = "Ź";
+var letters = ["A","Ą","B", "C", "Ć","D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N","Ń", "O","Ó", "P","Q","R", "S","Ś", "T", "U", "V","W", "X", "Y", "Z", "Ź", "Ż"];﻿
 
 String.prototype.setCharacter = function(place, character)
 		{
